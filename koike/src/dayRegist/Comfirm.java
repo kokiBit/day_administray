57879d376f7fcfeaa7d90b21aade4289bc8717bc
kokiBit <koki.takahashi@seattleconsulting.co.jp>
2014-06-10 14:19:33 +0900

    ファイルとデータの持たし方。

--- koike/src/dayRegist/Comfirm.java
+++ koike/src/dayRegist/Comfirm.java
@@ -2,25 +2,27 @@ package dayRegist;
 
 public class Comfirm implements Menu  {
 
+	private DumpFile dumpFile;
 	public void go() {
 
 		
 		
 		
 		
-		
+		dumpFile.create(file);
 	}
 
-	public void setDumpFile(String file) {
-
-		DumpFile dump = new DumpFile();
-
-	if(file.equals(null) ) {
-		dump.create(file);
-	}
-
-	
-
+	public void setDumpFile(DumpFile file) {
+		this.dumpFile = file;
+//
+//		DumpFile dump = new DumpFile();
+//
+//	if(file.equals(null) ) {
+//		dump.create(file);
+//	}
+//
+//	
+//
 	}
 
 
--- koike/src/dayRegist/Main.java
+++ koike/src/dayRegist/Main.java
@@ -3,7 +3,8 @@ package dayRegist;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
@@ -13,12 +14,15 @@ public class Main {
 	 */
 	public static void main(String[] args) throws IOException {
 		// TODO 自動生成されたメソッド・スタブ
-
+		final DumpFile f = new DumpFile();
 		Map<String, Menu> map = new HashMap<String, Menu>();
+		final Menu r = new Regist();
+		r.setDumpFile(f);
+		final Menu c = new Comfirm();
+		c.setDumpFile(f);
+		map.put("1", r);
 
-		map.put("1", new Regist());
-
-		map.put("2", new Comfirm());
+		map.put("2", c);
 
 		map.put("3", new Delete());
 

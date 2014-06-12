4ce483ce87a60496fc84ab0bc5ec44a50f0a7e95
kokiBit <koki.takahashi@seattleconsulting.co.jp>
2014-06-12 19:29:09 +0900

    チェック作成

---
+++ koike/src/utile/Check.java
@@ -0,0 +1,16 @@
+package utile;
+
+public class Check {
+
+	public static String MATCH_ASCII = "":
+
+	public boolean dayCeck(String str) {
+
+
+		if (str.matches(MATCH_ASCII)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
